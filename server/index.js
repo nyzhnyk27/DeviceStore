0497e2066b2086b1245292f2a5443a07876aa865
Oleh27 <oleh.work27@gmail.com>
2022-11-22 20:41:01 +0200

    Created data models and connection between them

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 require("dotenv").config()
 const express = require("express")
 const sequelize = require("./db")
+const models = require("./models/models")
+const cors = require("cors")
 
 const PORT = process.env.PORT  || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+// перевірка запитів через браузер
+app.get("/", (req, res) => {
+    res.status(200).json({message: "WORKING!"})
+})
+
 
 // функція для підключення БД
 const start = async () => {
