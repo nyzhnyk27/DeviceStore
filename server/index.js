fd93811fe25672b5af3c6a58d3f85a02b91922c6
Oleh27 <oleh.work27@gmail.com>
2022-11-23 17:58:03 +0200

    CRUD for types, brands, devices

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require("express")
 const sequelize = require("./db")
 const models = require("./models/models")
 const cors = require("cors")
+const fileUpload = require("express-fileupload")
+const router = require("./routes/index")
 const errorHandler = require("./middleware/ErrorHandlingMiddleware")
 
 const PORT = process.env.PORT  || 5000
@@ -10,6 +12,7 @@ const PORT = process.env.PORT  || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use("/api", router)
 
 
