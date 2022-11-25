9fbc33a20369158da7ab31b7f48f181463259f12
Oleh27 <oleh.work27@gmail.com>
2022-11-25 12:42:55 +0200

    Added serving static files

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,10 +1,9 @@
 const Router = require("express")
 const router = new Router()
 const brandController = require("../controllers/brandController")
-const checkRole = require("../middleware/checkRoleMiddleware")
 
 // метод post для створення брендів, get для отримання
-router.post("/", checkRole("ADMIN"), brandController.create)
+router.post("/", brandController.create)
 router.get("/", brandController.getAll)
 
 
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -2,7 +2,7 @@ const Router = require("express")
 const router = new Router()
 const deviceController = require("../controllers/deviceController")
 
-router.post("/", checkRole("ADMIN"), deviceController.create)
+router.post("/", deviceController.create)
 router.get("/", deviceController.getAll)
 router.get("/:id", deviceController.getOne)
 
